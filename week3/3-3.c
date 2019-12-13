dc8d71660be40a772104cd6b834cdd24af9596ac
wzjwzj-11 <55952256+wzjwzj-11@users.noreply.github.com>
2019-12-13 22:04:09 +0800

    week3

---
+++ week3/3-3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFFSIZE 8192
+
+int main()
+{
+	int n;
+	char buf[BUFFSIZE];
+	while((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+	{
+		if(write(STDOUT_FILENO, buf, n) != n)
+		{
+			printf("write error");
+			exit(-1);
+		}
+	}
+	if(n < 0)
+	{
+		printf("read error");
+		exit(-2);
+	}
+	exit(0);
+}
---
+++ week3/filehole.c
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+char buf1[] = "abcdefghij";
+char buf2[] = "ABCDEFGHIJ";
+
+int main()
+{
+	int fd;
+	if((fd = creat("file.hole", S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) < 0)
+		printf("creat error");
+
+	if(write(fd, buf1, 10) != 10)
+		printf("buf1 write error");
+	/* offset = 10 */
+
+	if(lseek(fd, 40, SEEK_SET) == -1)
+			printf("lseek error");
+		/* offset = 40 */
+	
+	if(write(fd, buf2, 10) != 10)
+		printf("buf2 write error");
+	/* offset = 50 */
+
+	exit(0);
+}
